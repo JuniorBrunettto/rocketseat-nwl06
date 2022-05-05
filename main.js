92d5dfdb71d6f3d65a050956dcf13bc1ff5059ae
JuniorBrunetto <clpebrjr@gmail.com>
2022-05-05 11:52:24 -0300

    Aula 04 - Landing

--- main.js
+++ main.js
@@ -18,10 +18,11 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando der scroll */
-const header = document.querySelector("#header")
-const navHeigth = header.offsetHeight
 
-window.addEventListener("scroll" , function(){
+function changeHeaderWhenScroll() {
+    const header = document.querySelector("#header")
+    const navHeigth = header.offsetHeight
+
     if(window.scrollY >= navHeigth){
         // scroll é maior que a altura do header
         header.classList.add("scroll")
@@ -29,7 +30,9 @@ window.addEventListener("scroll" , function(){
         // scroll menor que a altura do header
         header.classList.remove("scroll")
     }
-})
+}
+
+
 
 /* testimonials slider swiper 
 
@@ -60,17 +63,37 @@ const swiper = new Swiper('.swiper', {
   })
 
   /*  ScrollReveal mostrar elementos quando der review na tela */
-const ScrollReveal = ScrollReveal ({
+const scrollReveal = ScrollReveal ({
     origin: "top",
     distance: "30px",
     duration: 700,
     reset: true
 });
 
-ScrollReveal.reveal(`
+scrollReveal.reveal(`
 #home .image, #home .text,
 #about .image, #about .text
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials
-#contact .text, #contact .links
- `, {interval: 100});
+#contact .text, #contact .links,
+footer .brand, footer .social
+ `, {interval: 100})
+
+
+ /* botão voltar para o top */
+ 
+ function backToTop () {
+     const backToTopButton = document.querySelector(".back-to-top")
+
+     if(window.scrollY >= 500) {
+         backToTopButton.classList.add("show")
+     } else {
+         backToTopButton.classList.remove("show")
+     }
+
+ }
+/* When Scroll */
+ window.addEventListener("scroll" , function(){
+    changeHeaderWhenScroll()
+    backToTop()
+})
